e7195c0754db827a7a90806ba8f2407896ce4d20
Dragan <dragan@u.yale-nus.edu.sg>
2022-12-07 13:29:58 +0100

    changed code to download highest quality MP3, added subfoldering

--- playlist_downloader.py
+++ playlist_downloader.py
@@ -1,35 +1,37 @@
 import os
 import mutagen
-from mutagen.easyid3 import EasyID3
 from pytube import Playlist
 
 
-def yt_downloader(link):
+def downloader(links):
 
-    playlist = Playlist(link)
-    for video in playlist.videos:
-        new_file = video.title + "-" + video.author[:-8] + ".mp3"
-        print("Saving: " + new_file)
-        out_file = video.streams.filter(only_audio=True).first().download()
-        os.rename(out_file, new_file)
-        try:
-            meta = EasyID3(new_file)
-        except:
-            meta = mutagen.File(new_file, easy=True)
-            meta.add_tags()
-        meta["title"] = video.title
-        meta["artist"] = video.author[:-8]
-        meta.save(new_file)
-        print(new_file + " saved.")
-
-def main():
-    links = []
     for link in links:
         owd = os.getcwd()
-        os.mkdir(link.title)
-        os.chdir(link.title)
-        yt_downloader(link)
+        playlist = Playlist(link)
+        os.mkdir(playlist.title)
+        os.chdir(playlist.title)
+
+        for video in playlist.videos:
+            out_file = video.streams.filter(only_audio=True,
+                                            subtype="mp4").order_by("abr").last().download()
+            new_file = out_file[:-4] + ".mp3"
+            os.rename(out_file, new_file)
+
+            try:
+                meta = mutagen.easyid3.EasyID3(new_file)
+            except:
+                meta = mutagen.File(new_file, easy=True)
+                meta.add_tags()
+
+            meta["title"] = video.title
+            meta["artist"] = video.author[:-8]
+
+            meta.save(new_file)
+            print(new_file + " saved.")
+
         os.chdir(owd)
 
 if __name__=="__main__":
-    main()
+    links = []
+
+    downloader(links)
